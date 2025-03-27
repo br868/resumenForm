d7b696238f9c4b6613827a6cc1b2aaef22c9b0f6
br868 <bienvenida.86@gmail.com>
2025-03-27 00:57:15 -0400

    Update pag3.js

--- pag3.js
+++ pag3.js
@@ -1,10 +1,10 @@
 function siguientePag(page) {  
     const enfermedad = document.getElementById('enfermedad').value;  
-    localStorage.setItem('enfermedad', enfermedad); // Guardar dato en localStorage 
+    localStorage.setItem('enfermedad', enfermedad);
     const tiempo = document.getElementById('tiempo').value;  
-    localStorage.setItem('tiempo', tiempo); // Guardar dato en localStorage  
+    localStorage.setItem('tiempo', tiempo);  
     const detalle = document.getElementById('detalle').value;  
-    localStorage.setItem('detalle',detalle); // Guardar dato en localStorage   
+    localStorage.setItem('detalle',detalle);  
     window.location.href = page; // Navegar a la siguiente página  
 }  
 function atras(page) {  
